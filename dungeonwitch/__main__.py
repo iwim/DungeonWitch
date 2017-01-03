7afea9b30377fe240833e8d73b1d9d7d55d311ea
iwim <cybrqueen@gmail.com>
2017-01-02 21:52:14 -0800

    Added running window

--- dungeonwitch/__main__.py
+++ dungeonwitch/__main__.py
@@ -1,6 +1,24 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import pygame
+from pygame.locals import *
 
-print("Dungeon Witch!")
-sys.exit(0)
+pygame.init()
+background_colour = (255,255,255)
+(width, height) = (1000, 500)
+screen = pygame.display.set_mode((width, height))
+
+
+
+screen.fill(background_colour)
+pygame.display.set_caption("Dungeon Witch")
+pygame.display.flip()
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+sys.exit()
